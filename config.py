7519de743d24b5da892060ea9e41ea94226df325
Aritra Mahatma <128917736+aritra2023@users.noreply.github.com>
2025-08-14 10:45:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # API credentials
-API_ID = 123456
-API_HASH = "DWSC65HGFswcf56g8ergjhgdw54586"
-BOT_TOKEN = "123456:abcdekgfiJLKMNOPqjngghdf"
+API_ID = 14619078
+API_HASH = "c90576cc470b4bd4dc08396cfa449833"
+BOT_TOKEN = "8022266910:AAF_YsEoSZgWO6rcH3mGQqMCLs6nize0O28"
 
 # Redis configuration
 REDIS_CONFIG = {
@@ -13,11 +13,64 @@ REDIS_CONFIG = {
 }
 
 # Chat ID where videos will be stored
-PRIVATE_CHAT_ID = -100123456789
+PRIVATE_CHAT_ID = -1002880609847
 
 # Authentication cookie
 COOKIE = {
-    "COOKIE": "Paste your cookie here"
+    "COOKIE": "[
+    {
+        "domain": ".dm.1024terabox.com",
+        "expirationDate": 1786648168,
+        "hostOnly": false,
+        "httpOnly": false,
+        "name": "__stripe_mid",
+        "path": "/",
+        "sameSite": "strict",
+        "secure": true,
+        "session": false,
+        "storeId": null,
+        "value": "f8457e33-51b0-4a79-9688-084b77f761806ae573"
+    },
+    {
+        "domain": ".1024terabox.com",
+        "expirationDate": 1762888160,
+        "hostOnly": false,
+        "httpOnly": false,
+        "name": "_rdt_uuid",
+        "path": "/",
+        "sameSite": "strict",
+        "secure": true,
+        "session": false,
+        "storeId": null,
+        "value": "1755110923673.9c3a1772-54ba-4032-b1ff-1773d309f136"
+    },
+    {
+        "domain": ".dm.1024terabox.com",
+        "expirationDate": 1755113968,
+        "hostOnly": false,
+        "httpOnly": false,
+        "name": "__stripe_sid",
+        "path": "/",
+        "sameSite": "strict",
+        "secure": true,
+        "session": false,
+        "storeId": null,
+        "value": "32fc37a0-5ea4-4db8-8f8c-1a6a7f2e46796751d7"
+    },
+    {
+        "domain": ".1024terabox.com",
+        "expirationDate": 1786646941.073114,
+        "hostOnly": false,
+        "httpOnly": true,
+        "name": "ndus",
+        "path": "/",
+        "sameSite": null,
+        "secure": true,
+        "session": false,
+        "storeId": null,
+        "value": "YeAdchpteHuizMH9cUXKXfiex8V2KmfmvmMq2jfC"
+    }
+]"
 }
 # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
 
